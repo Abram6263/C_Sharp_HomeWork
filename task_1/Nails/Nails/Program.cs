7f020b120569313029ba3fa7c78257716fb4a2df
Abram6263 <iphoneneo@mail.ru>
2021-07-16 00:54:37 +0300

    first commit, only code

---
+++ task_1/Nails/Nails/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Nails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount, hits; // amount - кол-во гвоздей, hits - удары
+            Random rnd = new Random();
+            amount = rnd.Next(1, 11);
+            
+            for (int i = 1; i <= amount; i++)
+            {
+                Console.Write($"Гвоздь номер {i}:");
+                hits = rnd.Next(1, 6);
+                for (int j = 1; j <= hits; j++)
+                {
+                    Console.Write(" стук");
+                }
+                Console.WriteLine(" Гвоздь забит!!!");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
