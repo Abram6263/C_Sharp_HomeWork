c1d61ecf077d21da47316e8fecb62783273dc02b
Abram6263 <iphoneneo@mail.ru>
2021-07-23 14:47:58 +0300

    main task

---
+++ task_2/MyFirstSolution/MyFirstConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyFirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            Console.WriteLine(n);
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task_2/MyFirstSolution/MySecondConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MySecondConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.141592653;
+            decimal e = 1.414213562373095048M;
+
+            Console.WriteLine($"Значение pi = {pi} \nЗначение e = {e}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
