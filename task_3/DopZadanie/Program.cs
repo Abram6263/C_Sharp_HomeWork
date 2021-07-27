51925de27cf2f88e0782f7f2b74eed6c2397ed7e
Abram6263 <iphoneneo@mail.ru>
2021-07-27 15:24:00 +0300

    2 projects in 1 solution

---
+++ task_3/DopZadanie/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DopZadanie
+{
+    class Program
+    {
+        static int Delit (int a, int b)
+        {
+            return a % b;
+        }
+
+        // сделал для двух чисел так как не понимаю как математически рассчитывается нод для большего кол-ва чисел
+        static void Main(string[] args)
+        {
+
+            int x, y;
+            int ost;
+
+            Console.Write("Введите первое число: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            y = int.Parse(Console.ReadLine());
+            if (x > y)
+            {
+                ost = Delit(x, y);
+                x = y;
+                y = ost;
+            }
+            else
+            {
+                ost = Delit(y, x);
+                y = ost;
+            }
+
+            while(true)
+            {
+                ost = Delit(x, y);
+                x = y;
+                if(ost > 0)
+                {
+                    y = ost;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"НОД = {y}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task_3/task_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // задание 1 -----------------------------------------------------------------
+            const double pi = 3.14;
+            double r, l, S;
+
+            Console.Write("Введите радиус окружности: ");
+            r = double.Parse(Console.ReadLine());
+            Console.Write("Введите образующую окружности: ");
+            l = double.Parse(Console.ReadLine());
+            S = pi * r * (r + l);
+            Console.WriteLine($"Площадь окружности равна {S}");
+
+            // задание 2 --------------------------------------------------------------------
+            Console.WriteLine("------------------задание 2-----------------------------------------");
+            int x = 14, y = 1, z = 5;
+            x += y - x++ * z;
+            Console.WriteLine($"Переменная x: {x} \nПеременная y: {y} \nПеременная z: {z}");
+            z = --x - y * 5;
+            Console.WriteLine($"\n\nПеременная x: {x} \nПеременная y: {y} \nПеременная z: {z}");
+            y /= x + 5 % z;
+            Console.WriteLine($"\n\nПеременная x: {x} \nПеременная y: {y} \nПеременная z: {z}");
+            z = x++ + y * 5;
+            Console.WriteLine($"\n\nПеременная x: {x} \nПеременная y: {y} \nПеременная z: {z}");
+            x = y - x++ * z;
+            Console.WriteLine($"\n\nПеременная x: {x} \nПеременная y: {y} \nПеременная z: {z}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
