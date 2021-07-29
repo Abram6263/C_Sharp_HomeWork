675a3494351cc6f983c82ec42b0f2ed1efe42dc7
Abram6263 <iphoneneo@mail.ru>
2021-07-29 20:34:55 +0300

    commit

---
+++ task_4/zadanie_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace zadanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Введите число: ");
+            num = int.Parse(Console.ReadLine());
+            if (num >= 0 && num <= 30)
+            {
+                Console.WriteLine("Число попало в промежуток от 0 до 30");
+            }
+            else if (num >= 31 && num <= 60)
+            {
+                Console.WriteLine("Число попало в промежуток от 31 до 60");
+            }
+            else if (num >= 61 && num <= 100)
+            {
+                Console.WriteLine("Число попало в промежуток от 61 до 100");
+            }
+            else
+            {
+                Console.WriteLine("Я такого числа не знаю!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ task_4/zadanie_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace zadanie_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "";
+            int num1 = 201;
+            while (num1 < 500)
+            {
+                if (num1 % 17 == 0)
+                {
+                    s1 += Convert.ToString(num1 + ", ");
+                }
+                num1++;
+            }
+
+            Console.WriteLine(s1.Remove(s1.Length - 2));
+
+            Console.ReadKey();
+        }
+    }
+}
