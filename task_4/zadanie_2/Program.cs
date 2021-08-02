aa122b5bee1de22c7bc0f193074f9205d60746f6
Abram6263 <iphoneneo@mail.ru>
2021-08-03 02:10:01 +0300

    Переделал задание 2 в дз 4 через StringBuilder

--- task_4/zadanie_2/Program.cs
+++ task_4/zadanie_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace zadanie_2
 {
@@ -6,18 +7,23 @@ namespace zadanie_2
     {
         static void Main(string[] args)
         {
-            string s1 = "";
+            //string s1 = "";
+            var s1 = new StringBuilder();
+            string s2;
             int num1 = 201;
             while (num1 < 500)
             {
                 if (num1 % 17 == 0)
                 {
-                    s1 += Convert.ToString(num1 + ", ");
+                    s1.Append(num1.ToString() + ", ");
+                    //s1 += Convert.ToString(num1 + ", ");
                 }
                 num1++;
             }
-
-            Console.WriteLine(s1.Remove(s1.Length - 2));
+            s1.Remove(s1.Length - 2, 2);
+            s2 = s1.ToString();
+            Console.WriteLine(s2);
+            //Console.WriteLine(s1.Remove(s1.Length - 2));
 
             Console.ReadKey();
         }
