c7567577a9bab77d6ac5fa9643d4793ab5314cc8
Abram6263 <iphoneneo@mail.ru>
2021-08-29 17:29:31 +0300

    commit

---
+++ task_10/task_10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace task_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList list1 = new MyList();
+            var item1 = new MyItem() { Value = 10 };
+            var item2 = new MyItem() { Value = 20 };
+            list1.Add(item1);
+            list1.Add(item2);
+            foreach(var i in list1)
+            {
+                Console.WriteLine(i.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
